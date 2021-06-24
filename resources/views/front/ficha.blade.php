b06cc0f7ff876d4b29c7ce35b1b316af00fcfe9c
unknown <wellingthon957@gmail.com>
2021-06-24 07:16:45 -0300

    Firt commit

--- resources/views/front/ficha.blade.php
+++ resources/views/front/ficha.blade.php
@@ -7,18 +7,36 @@
 <div class="card">
 <div class="card-header"></div>
     <div class="card-body">
-        <h1>FICHA DE CATALOGRAFIA</h1>
-        <p style="font-size: 12pt; text-align: justify;" >Ficha catalográfica é um documento que reúne os dados que descrevem uma publicação, de forma padronizada - esse padrão é internacional.
-            <br>Ela aparece no verso da página de rosto das publicações, oficializando seus dados descritivos. 
-            Além de padronizar a catalogação da obra, a ficha catalográfica facilita a busca de informações sobre o material.</p><br>
-            <p>Para solicitar click na imagem abaixo.</p>
-   
-            <div class="form-group row">
-                <div class="content-header-options-1 row-fluid">
-                    <a href="https://forms.gle/zNMFznNKx6A1RDig6" target="_blanck">
-                    <img style="max-width: 60%; border-radius: 10px;box-shadow: 5px 0px 7px 2px #888888;" src="{{ asset('images/front_images/ficha.png') }}" alt=""></a>
-            </div>
-        </div>
+        <table class="tabelaConsultas">
+            <thead>
+                <tr>
+                    <th style="width: 20px; min-width: 20px;">
+                    </th>
+                    <th id="thCabecalho1" style="" class="ordenacao1" onclick="">FICHA DE CATALOGRAFIA
+                        <div class="setasOrdenacao"></div>
+                    </th>
+                </tr>
+            </thead>
+    <tbody>
+        <tr onclick="">
+            <td onclick="event.stopPropagation();">
+                <a id="" title="Visualizar em nova guia" class="link abrirDocNovaAbaConsulta" href="">
+                    <span class="icon novaAbaEscuro"></span>
+                </a>
+            </td>
+        <td style="" class="colunaID coluna0"><p style="text-align: justify">FLIEEN 02-04: Ficha catalográfica é um documento que reúne os dados que descrevem uma publicação, de forma padronizada - esse padrão é internacional.</p>
+            <p style="text-align: justify">FLIAAA 02-04: Ela aparece no verso da página de rosto das publicações, oficializando seus dados descritivos. 
+                Além de padronizar a catalogação da obra, a ficha catalográfica facilita a busca de informações sobre o material.
+                Para solicitar click na imagem abaixo.</p>
+    </tbody>
+</table><br>
+    <p>Para solicitar click na imagem abaixo.</p>
+    <div class="form-group row">
+        <div class="content-header-options-1 row-fluid">
+            <a href="https://forms.gle/zNMFznNKx6A1RDig6" target="_blanck">
+            <img style="max-width: 60%; border-radius: 10px;box-shadow: 5px 0px 7px 2px #888888;" src="{{ asset('images/front_images/ficha.png') }}" alt=""></a>
+    </div>
+</div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/front_layout/front_sidebar.blade.php
+++ resources/views/layouts/front_layout/front_sidebar.blade.php
@@ -4,6 +4,17 @@
 			<nav class="menu-de-apoio span9">
 	<h2 class="hide">Menu de Relevância </h2>
 </nav>
+
+
+
+<nav class="principal span9 closed">
+	<h2>Administração<i class="icon-chevron-up visible-phone visible-tablet pull-right"></i></h2>
+	<ul>
+		<li class="item-336"><a href="{{ url('admin')}}">Login</a></li>
+		<li class="item-343"><a href="">Em manunteção</a></li></ul>
+</nav>
+
+
 <nav class="principal span9 closed">
 	<h2>Serviços<i class="icon-chevron-up visible-phone visible-tablet pull-right"></i></h2>
 <ul>
@@ -26,7 +37,9 @@
 <a href="{{ url('product/boletim')}}">Boletim Informativo</a></li><li class="item-325">				
 </nav>
 	<nav class="principal span9 closed">
-	<h2>Serviços <i class="icon-chevron-up visible-phone visible-tablet pull-right"></i></h2><!-- visible-phone visible-tablet -->
+	<h2>Serviços
+		<i class="icon-chevron-up visible-phone visible-tablet pull-right"></i>
+	</h2>
 <ul>
 <li class="item-281">
 <a href="">Depósito de Teses e Dissertações</a></li><li class="item-282">
@@ -41,8 +54,7 @@
 	<li class="item-284"><a href="">Aquisição de acervos</a></li></ul>
 	</nav>
 <nav class="principal span9 closed">
-	<h2><a href="saude-literaria">Saúde literaria<i class="icon-chevron-up visible-phone visible-tablet pull-right"></i></h2><!-- visible-phone visible-tablet -->
-	</a>
+	<h2><a href="saude-literaria">Saúde literaria<i class="icon-chevron-up visible-phone visible-tablet pull-right"></i></h2></a>
 </nav>
 	{{-- <span class="hide">Fim do menu principal</span> --}}
 </section>

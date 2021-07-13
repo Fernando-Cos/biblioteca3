20bcc9ce9aa411c4f343a6dbea08b015ce54cd47
unknown <wellingthon957@gmail.com>
2021-07-13 15:58:51 -0300

    Alter From login

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -29,7 +29,7 @@
       @if(Session::has('error_message'))
         <div class="alert alert-warning" role="alert">
           {{ Session::get('error_message') }}
-          Erro[]
+          Erro
         </div>
       @endif
 
@@ -44,60 +44,55 @@
         </ul>
     </div>
 @endif
-      
-
-
-      <form action="{{ url('/admin') }}" method="post">@csrf
-        <div class="input-group mb-3">
-          <input name="email" id="email" type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+  <form action="{{ url('/admin') }}" method="post">@csrf
+    <div class="input-group mb-3">
+      <input name="email" id="email" type="email" class="form-control" placeholder="Email">
+      <div class="input-group-append">
+        <div class="input-group-text">
+          <span class="fas fa-envelope"></span>
         </div>
-        <div class="input-group mb-3">
-          <input name="password" id="password" type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
+      </div>
+    </div>
+    <div class="input-group mb-3">
+      <input name="password" id="password" type="password" class="form-control" placeholder="Password">
+      <div class="input-group-append">
+        <div class="input-group-text">
+          <span class="fas fa-lock"></span>
         </div>
-        <div class="row">
-          <div class="col-8">
-            {{-- <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div> --}}
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-8">
+        <div class="icheck-primary">
+          <input type="checkbox" id="remember">
+          <label for="remember">
+            Lembre - se
+          </label>
         </div>
-      </form>
-
-      {{-- <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
+      </div>
+      <!-- /.col -->
+      <div class="col-4">
+        <button type="submit" class="btn btn-primary btn-block">Acessar</button>
+      </div>
+      <!-- /.col -->
+    </div>
+  </form>
+      <div class="social-auth-links text-center mb-3">
+        <p>Outras Opçoes</p>
         <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+          <i class="fab fa-facebook mr-2"></i> Acessar através do facebook
         </a>
         <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+          <i class="fab fa-google-plus mr-2"></i> Acessar através do Google+
         </a>
-      </div> --}}
+      </div>
       <!-- /.social-auth-links -->
-
-      {{-- <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
+      <p class="mb-1">
+        <a href="forgot-password.html">Esqueci minha senha</a>
       </p>
       <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p> --}}
+        <a href="register.html" class="text-center">Registrar um novo usúario</a>
+      </p>
     </div>
     <!-- /.login-card-body -->
   </div>

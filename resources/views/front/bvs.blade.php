12978e59cb72e6829e2f5c8bc9b00a596710223a
Fernando Wellington <02821337205@saude.am.gov.br>
2021-07-22 11:28:34 -0400

    Alter from layout

--- resources/views/front/bvs.blade.php
+++ resources/views/front/bvs.blade.php
@@ -36,8 +36,23 @@
                 <p style="text-align: justify" >FLIEEN 02-04: Biblioteca Virtual em Saúde do Adolescente - <a href=" http://www.adolec.uerj.br/" target="_blank">Literatura Científica e Técnica.</a></p>
                 <p style="text-align: justify" >FLIEEN 02-04: BVS Enfermagem - <a href="https://pesquisa.bvsalud.org/enfermeria/" target="_blank">Portal de Pesquisa da BVS Enfermagem Informação e Conhecimento para a Saúde.</a></p>
                 <p style="text-align: justify" >FLIEEN 02-04: BVS Instituto Evandro Chagas - <a href="http://bvs.iec.gov.br/iec/" target="_blank">BVS - Instituto Evandro Chagas.</a></p>
-
-    
+                <p style="text-align: justify" >FLIEEN 02-04: Rede de Informação e Conhecimento | BVS-RIC - <a href="https://pesquisa.bvsalud.org/enfermeria/" target="_blank">Portal de Pesquisa da BVS Enfermagem Informação e Conhecimento para a Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Envelhecimento - <a href="https://pesquisa.bvsalud.org/enfermeria/" target="_blank">Portal de Pesquisa da BVS Enfermagem Informação e Conhecimento para a Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS História do Patrimônio Cultural e das Ciências da Saúde - HPCS - <a href="https://pesquisa.bvsalud.org/enfermeria/" target="_blank">Portal de Pesquisa da BVS Enfermagem Informação e Conhecimento para a Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Carlos Chagas - <a href="http://www.bvschagas.coc.fiocruz.br/php/level.php?lang=pt&component=40&item=1" target="_blank">BVS Carlos Chagas.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Bioética e Diplomacia em Saúde - <a href="https://bvsbioeticaediplomacia.fiocruz.br/" target="_blank">Bioética e Diplomacia em Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Saúde Reprodutiva e da Mulher - <a href="https://perinatal.bvsalud.org/" target="_blank">OPS.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Determinantes Sociais em Saúde - <a href="http://bvsdss.icict.fiocruz.br/" target="_blank">BVS Determinantes Sociais em Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Desenvolvimento Sustentável e Saúde Regional - <a href="http://dss.bvsalud.org/php/index.php" target="_blank">Desarrollo Sostenible y Salud.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Doenças Infecciosas e Parasitárias - <a href="https://www.bvsdip.icict.fiocruz.br/" target="_blank">Doenças Infecciosas e Parasitárias.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Economia da Saúde - <a href="https://economia.saude.bvs.br/" target="_blank">BVS - Economia da Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Educação em Ciências da Saúde - <a href="http://www.bvs.fm.usp.br/php/index.php" target="_blank">Biblioteca Virtual de Educação em Ciências da Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Homeopatia -<a href="https://homeopatia.bvs.br/" target="_blank">BVS Homeopatia.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Integralidade - <a href="https://www.bvsintegralidade.icict.fiocruz.br/" target="_blank">BVS Integralidade.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Odontologia - <a href="https://odontologia.bvs.br/" target="_blank">BVS Odontologia.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Prevenção e Controle do Câncer - <a href="https://controlecancer.bvs.br/" target="_blank">Prevenção e Controle de Câncer.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Violência e Saúde - <a href="https://www.bvsvs.icict.fiocruz.br/" target="_blank">BVS Violência e Saúde.</a></p>
+                <p style="text-align: justify" >FLIEEN 02-04: BVS Campus Virtual de Saúde Brasil - <a href="https://pesquisa.bvsalud.org/cvsp/?output=site&lang=es&from=1&sort=&format=summary&count=20&fb=&page=1&filter%5Btype%5D%5B%5D=oba&filter%5Bnode%5D%5B%5D=brasil&q=&index=&where=ALL" target="_blank">Campus Virtual de Salud Pública.</a></p>
             </tbody>
     </table>
             <div class="form-group row">

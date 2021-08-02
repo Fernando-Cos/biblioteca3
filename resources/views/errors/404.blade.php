c77eeeb12f3da961b8aec6cd056938a62f303e68
Fernando Wellington <02821337205@saude.am.gov.br>
2021-08-02 15:46:38 -0400

    page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Erro, Pagina Não encontrada</title>
+</head>
+<body>
+    <section style="padding-top: 100px ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h1 style="font-size:12px;">404</h1>
+                    <h2>Pagina não encontrada</h2>
+                    <p>dsdsdsfd</p>
+                    <a href="/" class="btn btn-sucess"> Voltar</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</body>
+</html>
+
--- resources/views/front/memorial.blade.php
+++ resources/views/front/memorial.blade.php
@@ -7,10 +7,17 @@
 <div class="card">
 <div class="card-header"></div>
     <div class="card-body">
-        <h1>MEMORIAL</h1><BR>
             <div class="form-group row">
+                <div class="card card-body">
+                    <p align="justify"> <b> Mergulhe nessa história conosco!</b></p>
+                    A pagina do memorial virtual da Secretaria de Estado de Saúde do Amazonas foi pensada para que a historia da saude 
+                    <p> pública do Estado se mantenha viva, além de organizar a trajetória da historia da Secretaria de Estado de Saúde.<br>
+                      Lançado no dia XX de XXXX de 2021 pela SES-AM, contará com fases XXXXX.
+                  </div>
+                    <br>
+                    <h3 align="center" style="color:#05407b"> <b>Linha do tempo da saúde no Amazonas</h3>
+      
                 <div class="content-header-options-1 row-fluid">
-                    
             </div>
         </div>
     </div>

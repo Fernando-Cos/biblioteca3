3bf7a567494f80e1d2c0100480b68e3526715522
Fernando Wellington <02821337205@saude.am.gov.br>
2021-08-03 15:44:17 -0400

    Alter from treinamento

--- resources/views/layouts/front_layout/front_layout.blade.php
+++ resources/views/layouts/front_layout/front_layout.blade.php
@@ -26,7 +26,6 @@
 	<script src="{{ url('js/front_js/jquery-migrate.min.js') }}" type="text/javascript"></script>
 	<script src="{{ url('js/front_js/mod_banners/banner-rotativo-home.js') }}" type="text/javascript"></script>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-	
 	<div id="google_translate_element2"></div>
 	<script type="text/javascript"></script>
 	{{-- <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2"></script> --}}
@@ -35,35 +34,80 @@
 <div id="conteudo">
 <body class="font-lato">
 
+
+{{-- 
+	<div id="root">
+		<main class="drift-widget-chat-layout" role="main" aria-live="polite">
+			<style>
+				.drift-widget-chat-wrapper.keyboard-focused {
+				box-shadow:  0 0 0 2px #50626a;
+				border-radius:4px;
+          }
+			</style>
+			<div class="drift-widget-preload-fonts" aria-hidden="true">
+				<span>A</span>
+				<strong>B</strong>
+			</div>
+			<div class="drift-widget-chat-wrapper drift-widget-chat-wrapper__open" tabindex="0">
+				<header class="drift-widget-header drift-widget-header--collapsed"> 
+					<div class="drift-widget-header-top-line" style="background: rgb(80, 98, 106); color: rgb(255, 255, 255);">
+					</div>
+					<div class="drift-widget-header--content">
+						<div class="drift-widget-greeting">
+							<div class="drift-widget-greeting-agent-header">
+								<span>drift-widget-greeting-recipient-name</span>
+								<div class="drift-widget-recipient-avatar">
+									<div class="drift-widget-avatar" style="background-image: url("https://driftt.imgix.net/https%3A%2F%2Fdriftt.imgix.net%2Fhttps%253A%252F%252Fs3.amazonaws.com%252Fcustomer-api-avatars-prod%252F1854863%252Fdc99a71317e447098145742daf871fc3mt2z8g5ir7s7%3Ffit%3Dmax%26fm%3Dpng%26h%3D200%26w%3D200%26s%3D55ecaa97789772e200d996984cbd23d5?fit=max&fm=png&h=200&w=200&s=ea7aa20ea7dffab85eef86664bafcfed");">
+									</div>	
+								</div>
+							</div>
+							<div class="drift-widget-welcome-message-content">
+								<div>
+									<div class="drift-welcome-message-content--body">Como podemos ajudar? Estamos aqui!</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="drift-widget-conversation-history" aria-hidden="true">
+					<style>
+
+					</style>
+				</div>
+			</div>
+		</main>
+	</div>
+ --}}
+
+
+
+
+
+
+
+
 @include('layouts.front_layout.front_header')
 	<div class="row-fluid">
 		<div id="navigation" class="span3">
 			<a href="#" class="visible-phone visible-tablet mainmenu-toggle btn">
-
 @include('layouts.front_layout.front_sidebar')
-
 @yield('content')
 <div id="content" class="span9">
 	<section id="content-section">
-
 {{-- @include('layouts.front_layout.front_main') --}}
-
 {{-- 
  <div class="row-fluid module ">
 <div class="outstanding-header">  --}}
-
 {{-- @include('layouts.front_layout.front_section') --}}
-
 </main>
 @include('layouts.front_layout.front_footer')
-
-
 <!-- scripts principais do template -->
 <script src="{{ url('js/front_js/bootstrap.min.js') }}" type="text/javascript"></script><noscript>&nbsp;<!-- item para fins de acessibilidade --></noscript>
 <script src="{{ url('js/front_js/jquery.cookie.js') }}" type="text/javascript"></script><noscript>&nbsp;<!-- item para fins de acessibilidade --></noscript>
 <script src="{{ url('js/front_js/template.js') }}" type="text/javascript"></script><noscript>&nbsp;<!-- item para fins de acessibilidade --></noscript>
 <script src="{{ url('js/front_js/verde.js') }}" type="text/javascript"></script><noscript>&nbsp;<!-- item para fins de acessibilidade --></noscript>
 {{-- <script src="{{ url('js/front_js/jquery.lightbox-0.5.js') }}"></script> --}}
+
 </body>
 	</div>
 </html>

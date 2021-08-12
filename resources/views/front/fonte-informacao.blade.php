5cf89b9a6549794fed8b6dea0d3b19d788a97611
Fernando Wellington <02821337205@saude.am.gov.br>
2021-08-12 11:31:11 -0400

    Alter from layout

--- resources/views/front/fonte-informacao.blade.php
+++ resources/views/front/fonte-informacao.blade.php
@@ -34,7 +34,7 @@
                         <div class="row-fluid">
                             <div class="item-page">
                                 <span class="documentCategory">
-                                    <a href="/ultimas-noticias.html">Últimas notícias</a></span>
+                                    <a href="/ultimas-noticias.html" style ="color: #3b4b5bcc">Últimas notícias</a></span>
                                     <h1 class="secondaryHeading">
                                         <a href="/ultimas-noticias/140-guia-de-fontes-de-informacao.html"></a>
                                     </h1>
@@ -42,7 +42,7 @@
                             </ul>
                         <div class="content-header-options-1 row-fluid">
                     <div class="documentByLine span7">
-                <ul>
+                <ul>    
             <li class="documentPublished">Publicado: Segunda, 23 de Novembro de 2020, 14h42</li>
             <li class="documentModified">Última atualização em Sexta, 27 de Novembro de 2020, 09h29</li>
         </ul>
@@ -51,18 +51,209 @@
 <div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; width: 0px; height: 0px;"><div></div></div></div>
 <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=509544779073047";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script><noscript>&nbsp;<!-- item para fins de acessibilidade --></noscript>
 <!-- fim botão facebook -->	
+
+<!-- Listagem de fonte de informações  -->
+<div id="content" class="span9 internas">
+    <section id="content-section">
+    <span class="hide">Início do conteúdo da página</span>
+    <div class="row-fluid">
+        <div class="category-list">
+                <div class="content-header-options-1 row-fluid">
+                    <div class="documentByLine">
+                        <span class="documentAuthor">Escrito por <strong>Fernando Amaral costa</strong></span>
+                    </div>
+                </div>
+	        <div class="cat-items">
+        <form action="https://progesp.ufam.edu.br/publicacoes-e-noticias.html" method="post" name="adminForm" id="adminForm">
+	<div class="row-fluid" style="background-color: #0660179e; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+		<fieldset class="filters">
+            <div class="display-limit pull-right">
+            <label for="limit"> &nbsp;
+				<select id="limit" name="limit" class="inputbox input-mini" size="1" onchange="this.form.submit()">
+	<option value="5" selected="selected">5</option>
+	<option value="10">10</option>
+	<option value="15">15</option>
+	<option value="20">20</option>
+	<option value="25">25</option>
+	<option value="30">30</option>
+	<option value="50">50</option>
+	<option value="100">100</option>
+	<option value="0">Tudo</option>
+</select>
+</label>
+    </div>
+        <input type="hidden" name="filter_order" value="">
+        <input type="hidden" name="filter_order_Dir" value="">
+        <input type="hidden" name="limitstart" value="">
+    </fieldset>
+</div>
+<div class="tile-list-1" style="background-color: #e7e7e7; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+    <div class="tileItem">
+        <div class="span10 tileContent">
+            <div class="tileImage">
+                <a href="">
+                <img class="tileImage" src="/images/6dp.jpg" alt="" height="86" width="128" data-pagespeed-url-hash="2225517721" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">							
+            </a>
+        </div>
+        <span class="subtitle"></span>
+        <h2 class="tileHeadline">
+            <a href="">Drops Pedagógico – 6ª Edição: Você sabe o que é aprendizagem significativa?</a></h2>
+            <span class="description">
+            <p></p>
+        </span> 
+    </div>
+<div class="span2 tileInfo">
+    <ul>
+        <li class="hide">Escrito por Larissa</li>
+        <li class="hide">publicado</li>
+        <li><i class="icon-fixed-width icon-calendar"></i> 10/08/21</li>
+        <li><i class="icon-fixed-width icon-time"></i> 12h49</li>
+        <!-- <li><i class="icon-fixed-width"></i> Artigo</li> -->
+    </ul>							            								
+</div>									
+</div>
+			<!-- div.tileItem -->
+<div class="tileItem">
+    <div class="span10 tileContent">
+        <div class="tileImage">
+            <a href="/publicacoes-e-noticias/936-portaria-267-sobre-o-retorno-da-vigencia-dos-editais-dos-auxilios-de-2020-capital-e-2019-unidades-fora-da-sede.html">
+                <img class="tileImage" src="/images/DAEST/Portaria-267.png" alt="" height="86" width="128" data-pagespeed-url-hash="2375750621" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">							
+                </a>
+            </div>
+            <span class="subtitle"></span>
+            <h2 class="tileHeadline">
+                <a href="/publicacoes-e-noticias/936-portaria-267-sobre-o-retorno-da-vigencia-dos-editais-dos-auxilios-de-2020-capital-e-2019-unidades-fora-da-sede.html">DAEST informa retorno da vigência dos auxílios referentes aos editais de 2019 e 2020 da Assistência Estudantil / PNAES</a>
+            </h2>
+            <span class="description">
+                <p></p>
+            </span> 
+        </div>
+    <div class="span2 tileInfo">
+            <ul>
+                <li class="hide">Escrito por Ageu Monteiro Maia Júnior</li>
+                <li class="hide">publicado</li>
+                <li><i class="icon-fixed-width icon-calendar"></i> 04/08/21</li>
+                <li><i class="icon-fixed-width icon-time"></i> 11h50</li>
+            </ul>							            								
+        </div>									
+    </div>
+			<!-- div.tileItem -->
+        <div class="tileItem">
+            <div class="span10 tileContent">
+            <div class="tileImage">
+                <a href="/publicacoes-e-noticias/933-homologacao-de-inscritos-e-cronograma-dos-editais-2021-da-assistencia-estudantil-campus-manaus.html">
+                <img class="tileImage" src="/images/DAEST/Imagem-Homologao-2021.png" alt="" height="86" width="128" data-pagespeed-url-hash="143680081" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">							
+            </a>
+        </div>
+        <span class="subtitle"></span>
+        <h2 class="tileHeadline">
+            <a href="/publicacoes-e-noticias/933-homologacao-de-inscritos-e-cronograma-dos-editais-2021-da-assistencia-estudantil-campus-manaus.html">HOMOLOGAÇÃO DE INSCRITOS E CRONOGRAMA DOS EDITAIS 2021 DA ASSISTÊNCIA ESTUDANTIL / CAMPUS MANAUS</a></h2>
+            <span class="description">
+                <p>Em teste</p></span> 
+            </div>
+        <div class="span2 tileInfo">
+            <ul>
+                <li class="hide">Escrito por Ageu Monteiro Maia Júnior</li>
+                <li class="hide">publicado</li>
+                <li><i class="icon-fixed-width icon-calendar"></i> 30/07/21</li>
+                <li><i class="icon-fixed-width icon-time"></i> 17h31</li>
+                <!-- <li><i class="icon-fixed-width"></i> Artigo</li> -->
+            </ul>							            								
+        </div>									
+    </div>
+			<!-- div.tileItem -->
+	<div class="tileItem">
+		<div class="span10 tileContent">
+			<div class="tileImage">
+				<a href="/publicacoes-e-noticias/920-retificacao-do-cronograma-dos-editais-2021-campus-manaus.html">
+					<img class="tileImage" src="/images/RETIF1.jpg" alt="" height="86" width="128" data-pagespeed-url-hash="3685301882" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">							
+						</a>
+					</div>
+					<span class="subtitle"></span>
+					<h2 class="tileHeadline">
+	              		<a href="/publicacoes-e-noticias/920-retificacao-do-cronograma-dos-editais-2021-campus-manaus.html">Retificação do Cronograma dos Editais 2021 - Campus Manaus</a>
+	          		</h2>
+	          		<span class="description">
+	          			<p></p></span> 
+                    </div>
+				<div class="span2 tileInfo">
+					<ul>
+                    <li class="hide">Escrito por Larissa</li>
+						<li class="hide">publicado</li>
+						<li><i class="icon-fixed-width icon-calendar"></i> 14/07/21</li>
+						<li><i class="icon-fixed-width icon-time"></i> 22h37</li>
+						<!-- <li><i class="icon-fixed-width"></i> Artigo</li> -->
+					</ul>							            								
+				</div>									
+			</div>
+			<!-- div.tileItem -->
+		<div class="tileItem">
+			<div class="span10 tileContent">
+				<div class="tileImage">
+					<a href="/publicacoes-e-noticias/919-adendo-aos-editais-de-assistencia-estudantil-2021-todos-os-campus.html">
+						<img class="tileImage" src="/images/adendo1.jpg" alt="" height="86" width="128" data-pagespeed-url-hash="1149081677" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">							
+						</a>
+					</div>
+					<span class="subtitle"></span>
+					<h2 class="tileHeadline">
+	              		<a href="/publicacoes-e-noticias/919-adendo-aos-editais-de-assistencia-estudantil-2021-todos-os-campus.html">Adendo aos Editais de Assistência Estudantil 2021 - TODOS OS CAMPUS</a>
+	          		</h2>
+	          		<span class="description">
+	          			<p></p></span> 
+	          						</div>
+				<div class="span2 tileInfo">
+					<ul>
+                        <li class="hide">Escrito por Larissa</li>
+						<li class="hide">publicado</li>
+						<li><i class="icon-fixed-width icon-calendar"></i> 14/07/21</li>
+						<li><i class="icon-fixed-width icon-time"></i> 22h36</li>
+						<!-- <li><i class="icon-fixed-width"></i> Artigo</li> -->
+					</ul>							            								
+				</div>									
+			</div>
+			<!-- div.tileItem -->
+			</div>
+	</form></div>
+<div class="pagination">
+    <p class="counter pull-left">Pagina 1 de 32</p>
+    <ul>
+        <li class="pagination-start"><span class="pagenav">Iniciar</span></li>
+        <li class="pagination-prev"><span class="pagenav">Ant</span></li>
+        <li><span class="pagenav">1</span></li>			
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=5" class="pagenav">2</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=10" class="pagenav">3</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=15" class="pagenav">4</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=20" class="pagenav">5</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=25" class="pagenav">6</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=30" class="pagenav">7</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=35" class="pagenav">8</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=40" class="pagenav">9</a></li>
+        <li>
+            <a href="/publicacoes-e-noticias.html?start=45" class="pagenav">10</a></li>
+        <li class="pagination-next">
+            <a title="Próx" href="/publicacoes-e-noticias.html?start=5" class="hasTooltip pagenav">Próx</a></li>
+	    <li class="pagination-end">
+            <a title="Fim" href="/publicacoes-e-noticias.html?start=155" class="hasTooltip pagenav">Fim</a></li>
+        </ul>
+    </div>
+</div>
+</div>
+    <span class="hide">Fim do conteúdo da página</span>
+</section>
+</div>
 </div>
 </div>		 
 </div>
-
-
-
-
-
 <div class="below-content">
-    <div class="line">
-    registrado em:
-        <span><a class="link-categoria" rel="tag" href="/ultimas-noticias.html">Últimas notícias</a></span>
+    <div class="line">registrado em:<span><a class="link-categoria" rel="tag" href="/ultimas-noticias.html">Últimas notícias</a></span>
     </div>
 </div>
 </div>

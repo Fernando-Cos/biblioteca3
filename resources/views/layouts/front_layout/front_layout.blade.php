82b1608368dcc057cf1efe6a0969ee68c2ab0561
Fernando Wellington <02821337205@saude.am.gov.br>
2021-08-18 13:49:27 -0400

    Alter from chatBot

--- resources/views/layouts/front_layout/front_layout.blade.php
+++ resources/views/layouts/front_layout/front_layout.blade.php
@@ -47,7 +47,7 @@
 	<div class="chat_box" style="bottom: 11%;">
 		<div class="conv-form-wrapper">
 		<form action="" method="GET" class="hidden">
-			<select name="programmer" data-conv-question="So, are you a programmer? (this question will fork the conversation based on your answer)">
+			<select name="programmer" data-conv-question="Olá! Por aqui voçê pode estar tirando suas duvidas.">
 				<option value="yes">Yes</option>
 				<option value="no">No</option>
 			</select>
